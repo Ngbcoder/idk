57cc1b1608e51bcb5428fde203e0d2c6a3c3749e
Ngbcoder <112007432+Ngbcoder@users.noreply.github.com>
2024-11-13 14:42:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,29 @@ let Seled = 0
 input.onButtonPressed(Button.A, function () {
     Seled += 1
 })
+input.onButtonPressed(Button.B, function () {
+    Seled += -1
+})
+basic.forever(function () {
+    if (Seled == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (Seled == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
 basic.forever(function () {
     if (Seled == 3) {
         Seled = 1
